46f298a681ed9a27e2f563da7c2543afec6e6d98
Divyansh Agarwal <divyanshagarwal053@gmail.com>
2022-03-29 23:13:10 +0530

    added implememntation for diff.java

--- Diff.java
+++ Diff.java
@@ -2,7 +2,7 @@ public class Diff{
 
     public static void main(String[] args){
         int a = 10;
-        int b = 6;
+        int b = 9;
 
         System.out.println("The diffrence is: "+ (a-b));
 
